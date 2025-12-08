7c0e9c5155e346c0c72124c0f60b38b111967f92
koppen <stijn.koppen@gmail.com>
2025-12-08 15:18:55 +0100

    add centrifugal pump and example

---
+++ examples/centrifugal_pump_props.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from pumps import CentrifugalPump
+
+pump = CentrifugalPump()
+
+q0, h0 = pump.get_operating_points(100)
+
+z = np.linspace(0.5, 1.5, 5)
+w = pump.w0 * z
+
+fig, host = plt.subplots(figsize=(8,5), layout='constrained')
+
+ax2 = host.twinx()
+
+host.set_xlim([0, pump.qm0 * pump.cubpstolmin])
+host.set_ylim([0, z[-1]**2 * pump.hm0])
+ax2.set_ylim([0, 1])
+
+host.set_xlabel("Capacity [L/min]")
+host.set_ylabel("Head [m]")
+
+ax2.set_ylabel("Efficiency [-]")
+
+for i in w:
+    zi = i / pump.w0
+    q = zi * q0
+    hi = pump.hq(q, i)
+    host.plot(q * pump.cubpstolmin, hi, color="black", label=f"w/w0 = {zi:.2f}")
+host.legend()
+
+host.plot(q0 * pump.cubpstolmin, pump.hq0(q0))
+host.scatter(pump.q0p * pump.cubpstolmin, pump.h0p)
+
+ax2.plot(q0 * pump.cubpstolmin, pump.eff0(q0))
+ax2.scatter(pump.q0p * pump.cubpstolmin, pump.eff0p)
+
+plt.show()
--- pumps.py
+++ pumps.py
@@ -1,3 +1,60 @@
 """
 Lumped parameter models describing the analytical -- typically nonlinear but static -- behaviour of (centrifugal) pumps.
 """
+import numpy as np
+from abc import ABC, abstractmethod
+from utils import cubic_fit, quadratic_fit
+from math import pi
+from matplotlib import pyplot as plt
+
+class Pump(ABC):
+    g = 9.81  # gravitational acceleration
+    rho = 1000  # fluid density
+    rpmtoradps = (2 * pi) / 60 # conversion rate rpm to rad/s
+    radpstorpm = 1 / rpmtoradps # conversion rate rad/s to rpm
+    cubpstolmin = 60e3 # conversion rate m3/s to l/min
+    lmintocubps = 1 / cubpstolmin # conversion rate l/min to m3/s
+    gamma = rho * g
+
+class CentrifugalPump(Pump):
+
+    def __init__(self,
+                 hm0: float = 60,
+                 qn0: float = 40 / 60000,
+                 hn0: float = 45,
+                 qm0: float = 80 / 60000,
+                 w0: float = 1770 * (2 * pi / 60),
+                 effn: float = 0.35,
+                 kh: float = 1/100,
+                 kw: float = 1/1e6):
+        self.kh = kh # pump dynamic stiffness head residual
+        self.kw = kw # pump dynamic stiffness shaft speed
+
+        self.qn0 = qn0 # nominal capacity
+        self.hn0 = hn0 # nominal head
+
+        self.hm0 = hm0 # maximum head at zero capacity
+        self.qm0 = qm0 # maximum capacity at zero head
+        self.w0 = w0 # reference speed
+
+        self.q0p = np.array([0.0, self.qn0, self.qm0]) # capacity points for H-Q at w0
+        self.h0p = np.array([self.hm0, self.hn0, 0.0]) # head points for H-Q at w0
+        self.p0p = self.gamma * self.h0p
+
+        # self.hq0_coeff = quadratic_fit(self.q0p, self.h0p)
+        self.hq0_coeff = cubic_fit(self.q0p, self.h0p, 0, 0)
+        self.hq0 = np.poly1d(self.hq0_coeff) # H-Q curve at w0, h0(q0)
+        self.pq0 = self.gamma * self.hq0
+        self.hq = lambda q, w: sum([self.hq0[i] * (w / self.w0) ** (2 - i) * q ** (i) for i in range(len(self.hq0)+1)]) # H-Q curve at w, h(q, w)
+        self.pq = lambda q, w: self.gamma * self.hq(q, w)
+
+        self.pn0 = self.hn0 * self.gamma  # nominal pressure
+        self.nhp = self.qn0 * self.pn0  # nominal horse power
+        self.effn0 = effn # nominal efficiency (at reference speed)
+        self.nbp = self.effn0 * self.nhp
+
+        self.eff0p = np.array([0.0, self.effn0, 0.0]) # efficiency points for eta(q0)
+        self.eff0 = np.poly1d(cubic_fit(self.q0p, self.eff0p, 1, 0))
+
+    def get_operating_points(self, n: int = 100, tol: float = 1e-6):
+        return np.linspace(tol, self.qm0-tol, n), np.linspace(0.0, self.hm0, n)
