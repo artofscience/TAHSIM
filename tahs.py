57eef70915b5920f2292992dba4a157bc56217ae
koppen <stijn.koppen@gmail.com>
2026-01-26 15:48:21 +0100

    drastic changes to nonlinear membrane model and limo

--- tahs.py
+++ tahs.py
@@ -41,12 +41,16 @@ class PressureActuatedLinearMembrane(TAH):
         self.E = E
         self.V0 = V0
 
+    def fcn(self, dv):
+        return self.E * dv
+
     def pressure(self, volume: float, t: float) -> float:
         """
         Ventricular pressure via time-varying actuation pressure:
         Pv = E * (V - V0) + Pact(t)
         """
-        return self.E * (volume - self.V0) + self.Pact(t)
+        dv = self.V0 - volume
+        return  self.Pact(t) - self.fcn(dv)
 
     def pressure_diff(self, volume: float, flow: float, t: float) -> float:
         """
@@ -56,7 +60,7 @@ class PressureActuatedLinearMembrane(TAH):
         return self.E * flow + self.Pact.diff(t)
 
 class PressureActuatedNonlinearMembrane(TAH):
-    def __init__(self, Pact: TDP = TDP(min=-4, max=120), V0: float = 20,
+    def __init__(self, Pact: TDP = TDP(min=0, max=120), V0: float = 80,
                  a: float = 10,
                  b: float = 0.01,
                  c: float = -20,
@@ -78,26 +82,101 @@ class PressureActuatedNonlinearMembrane(TAH):
     d = 20
     """
 
+    def fcn(self, dv):
+        """
+        dp = fcn(dv)
+        """
+        tmp = self.b * (dv - self.c)
+        return self.a * np.log(tmp / (1 - tmp)) + self.d
+
     def pressure(self, volume: float, t: float) -> float:
-        """
-        Ventricular pressure via time-varying actuation pressure:
-        Pv = a * ln(z / (1-z)) + d + Pact(t), z = b * (dV - c)
-        """
-        dV = volume - self.V0
-        tmp = self.b * (dV - self.c)
-        Pv = self.a * np.log(tmp / (1 - tmp)) + self.d
-        Pv += self.Pact(t)
-        return Pv
+        dV = self.V0 - volume
+        return self.Pact(t) - self.fcn(dV)
 
     def pressure_diff(self, volume: float, flow: float, t: float) -> float:
         """
         dPv/dt = dPv/dV * Q + dPact/dt
         Q = dV/dt (ventricular flow rate)
 
-        dPv/dV = a / (z * (1 - b * z)), z = dV - c
+        pv = a * ln(z/(1-z)) + d, z = b(dV - c)
+        dpv/dz = a / z(1-z), dz/dV = b
+        dpv/dV = ab / z(1-z)
         """
+        #dpv/df = -1
+        #d dv / dv = -1
+        # dpv/ dv = dpv / df * df / d dv * d dv / dv = df/ d dv
+        dV = self.V0 - volume
+        z = self.b * (dV - self.c)
 
-        dPvdV =  self.a / (volume * (1 - self.b * volume))
+        dPvdV =  self.a * self.b / (z * (1 - z))
 
-        return dPvdV * flow + self.Pact.diff(t)
+        return self.Pact.diff(t) + dPvdV * flow
 
+class LIMO(TAH):
+    def __init__(self, Pact: TDP = TDP(min=0, max=120), L: float = 0.017, N: int = 8, D: float = 0.05, H: float = 0.001, mu: float = 3e5):
+        self.Pact = Pact
+        # self.c = np.array([-2.26531554e+00,  1.61644772e+01,  1.01374824e-02, -3.77366149e+01,
+        #                    -3.00151200e+00,  1.06145026e+01,  2.87000461e+01,  9.13326808e-01])
+        self.c = np.array([-3.73674074, 26.34867036, 1.09021501, -59.92398559,
+               -5.03912252, 11.03755932, 42.90372121, 1.77124204])
+        # self.c = np.array([ -1.80115542,  12.81912422,  -0.39968068, -29.69849822,
+        # -2.1104906 ,  10.1370886 ,  23.2695619 ,   0.5423379 ])
+        self.mmhg2pa = 133
+        self.ml2m3 = 1e-6
+        self.mu = mu / self.mmhg2pa
+        self.L = L
+        self.H = H
+        self.N = N
+        self.D = D
+
+
+    def fcn(self, vc, pact):
+        # normalized function
+        # Pch = Pc N L / mu H = f[vcn, pan]
+        return self.c[0] + self.c[1] * vc + self.c[2] * pact + self.c[3] * vc**2 + self.c[4] * pact**2 + self.c[5] * vc * pact + self.c[6] * vc**3 + self.c[7] * pact**3
+
+    def pressure(self, vc: float, t: float) -> float:
+        # Pc = muH/NL * f[vc/NL2D, L/muH Pact]
+        vcn = vc * self.ml2m3 / self.N / self.L**2 / self.D
+        pactn = self.Pact(t) * self.L / self.mu / self.H
+        return self.mu * self.H * self.fcn(vcn, pactn) / self.N / self.L
+
+    def dfcndvc(self, vc, pact):
+        a, b, c, d, e, f, g, h = self.c
+        return b + 2 * d * vc + f * pact + 3 * g * vc**2
+
+    def dfcndpact(self, vc, pact):
+        a, b, c, d, e, f, g, h = self.c
+        return c + 2 * e * pact + f * vc + 3 * h * pact**2
+
+    def pressure_diff(self, vc: float, flow: float, t: float) -> float:
+        vcn = vc * self.ml2m3 / self.N / self.L ** 2 / self.D
+        pactn = self.Pact(t) * self.L / self.mu / self.H
+        return self.mu * self.H * (self.dfcndvc(vcn, pactn) * flow / self.N / self.L**2 / self.D * self.ml2m3 + self.dfcndpact(vcn, pactn) * self.Pact.diff(t) * self.L / self.mu / self.H) / self.N / self.L
+
+if __name__ == '__main__':
+
+    limo = LIMO(TDP(min=10, max=120))
+
+    plt.figure()
+    vc = np.linspace(0.1, 1.0, 100)
+    t = np.linspace(0.001, 2.5, 100)
+    for time in t:
+        plt.plot(vc, limo.fcn(vc, time))
+
+    plt.xlim([-0.1, 1])
+    plt.ylim([-0.1, 2.5])
+
+    plt.figure()
+    vc = np.linspace(10, 100, 100)
+    t = np.linspace(1, 140, 10)
+    for time in t:
+        plt.plot(vc, limo.pressure(vc, time))
+
+    plt.xlim([0, 100])
+    plt.ylim([-1, 100])
+
+    plt.xlabel(r'$v_\text{c}$')
+    plt.ylabel(r'$P_\text{c}$')
+
+    plt.show()
